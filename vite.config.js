e65e9a810e05378145b7b7bc0d221e4732e14902
Anna Kollberg <anko2300@student.miun.se>
2026-02-03 11:33:03 +0100

    lägger till automatisk konvertering av bilder till webP

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer"; 
 
 export default defineConfig({
   root: 'src',
@@ -13,5 +14,21 @@ export default defineConfig({
         process: resolve(__dirname, 'src/arbetsprocess.html')
       }
     }
-  }
+  },
+  plugins: [
+    ViteImageOptimizer({
+        jpg: {
+            quality: 75
+        },
+        jpeg: {
+            quality: 75
+        },
+        webp: {
+            quality: 75
+        },
+        avif: {
+            quality: 75
+        }
+    })
+  ]
 })
