a75f841a7804f2359f96b1a623dd4963f0bbe2b6
Anna Kollberg <anko2300@student.miun.se>
2026-03-10 12:35:14 +0100

    la till fethc anrop för att hämta kursdata

--- src/js/ramschema.js
+++ src/js/ramschema.js
@@ -1,2 +1,23 @@
 "use strict"; 
 
+let courses = []; // Array för att lagra kurserna
+
+async function loadCourses() { // Funktion som hämtar JSON-data
+
+    try {
+        // fetch-anrop till JSON-filen
+        const response = await fetch("https://webbutveckling.miun.se/files/ramschema.json");
+
+        // Konverterar svar till objekt
+        courses = await response.json();
+
+        console.log(courses);
+
+        // Datan skickas den till funktionen som visar kurserna
+        displayCourses(courses);
+
+    } catch (error) {
+
+        console.error("Fel vid hämtning:", error);
+    }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,35 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
-import { ViteImageOptimizer } from "vite-plugin-image-optimizer"; 
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
-  root: 'src',
-  build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/index.html'),
-        images: resolve(__dirname, 'src/bilder.html'),
-        process: resolve(__dirname, 'src/arbetsprocess.html')
-      }
-    }
-  },
-  plugins: [
-    ViteImageOptimizer({
-        jpg: {
-            quality: 75
-        },
-        jpeg: {
-            quality: 75
-        },
-        webp: {
-            quality: 75
-        },
-        avif: {
-            quality: 75
+    base: '/laboration-01/',
+    root: 'src',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'src/index.html'),
+                images: resolve(__dirname, 'src/bilder.html'),
+                process: resolve(__dirname, 'src/arbetsprocess.html')
+            }
         }
-    })
-  ]
+    },
+    plugins: [
+        ViteImageOptimizer({
+            jpg: {
+                quality: 75
+            },
+            jpeg: {
+                quality: 75
+            },
+            webp: {
+                quality: 75
+            },
+            avif: {
+                quality: 75
+            }
+        })
+    ]
 })
