d9ee1c040f2f2cc0f3e07780ca66fcf7adc1acc3
Anna Kollberg <anko2300@student.miun.se>
2026-05-04 16:07:19 +0200

    la till funktion för att visa kurser i tabell

--- src/js/ramschema.js
+++ src/js/ramschema.js
@@ -21,3 +21,24 @@ async function loadCourses() { // Funktion som hämtar JSON-data
         console.error("Fel vid hämtning:", error);
     }
 }
+
+function displayCourses(courseList) { // Funktion som skriver ut kurser i tabellen
+
+    const table = document.getElementById("courseTable");
+    table.innerHTML = ""; // Tömmer tabellen innan nya kurser läggs till 
+
+    courseList.forEach(course => {
+
+        const row = document.createElement("tr");
+
+        // Gör en rad för kurskod, namn och progression
+        row.innerHTML = `
+        <td>${course.code}</td>
+        <td>${course.coursename}</td>
+        <td>${course.progression}</td>
+        `;
+        table.appendChild(row);
+
+    });
+}
+loadCourses(); // Hämtar kurserna och visar dom när sidan laddas 
